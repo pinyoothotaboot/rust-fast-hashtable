08d674fda55da3e323fcb491f779b9bdbbb8a459
Pinyoo Thotaboot <pinyoo_too@hotmail.com>
2024-04-14 21:45:05 +0700

    Fixed sample

--- benches/matched_benchmark.rs
+++ benches/matched_benchmark.rs
@@ -6,7 +6,7 @@ use criterion::{
 };
 
 #[inline]
-fn matched(keys1 : &Vec<u8>,keys2 : &Vec<u8>) -> bool {
+fn matched(keys1 : Vec<u8>,keys2 : Vec<u8>) -> bool {
     let len_key1 = keys1.len();
     let len_key2 = keys2.len();
 
@@ -50,7 +50,7 @@ fn matched(keys1 : &Vec<u8>,keys2 : &Vec<u8>) -> bool {
 }
 
 #[inline]
-fn in_matched(keys1 : &Vec<u8>,keys2 : &Vec<u8>) -> bool {
+fn in_matched(keys1 : Vec<u8>,keys2 : Vec<u8>) -> bool {
     // Check length of hash key and store hash key.
     // When length of 2 keys has not match.Then return false
     
@@ -77,8 +77,8 @@ fn in_matched(keys1 : &Vec<u8>,keys2 : &Vec<u8>) -> bool {
 }
 
 #[inline]
-fn builtin_matched(keys1 : &Vec<u8>,keys2 : &Vec<u8>)  -> bool {
-    let matching = keys1.iter().zip(keys2).filter(|&(keys1,keys2)| keys1 == keys2).count();
+fn builtin_matched(keys1 : Vec<u8>,keys2 : Vec<u8>)  -> bool {
+    let matching = keys1.iter().zip(&keys2).filter(|&(keys1,keys2)| keys1 == keys2).count();
     matching == keys1.len()
 }
 
@@ -87,17 +87,17 @@ fn bench_matched(c : &mut Criterion) {
     let mut keys1 = String::from("A");
     let mut keys2 = String::from("A");
 
-    keys1.extend(vec!['H';1000].iter());
-    keys1.extend(vec!['K';1000].iter());
-    keys1.extend(vec!['D';1000].iter());
-    keys1.extend(vec!['O';1000].iter());
-    keys1.extend(vec!['R';1000].iter());
+    keys1.extend(vec!['H';1].iter());
+    keys1.extend(vec!['K';1].iter());
+    keys1.extend(vec!['D';1].iter());
+    keys1.extend(vec!['O';1].iter());
+    keys1.extend(vec!['R';1].iter());
 
-    keys2.extend(vec!['H';1000].iter());
-    keys2.extend(vec!['K';1000].iter());
-    keys2.extend(vec!['D';1000].iter());
-    keys2.extend(vec!['O';1000].iter());
-    keys2.extend(vec!['R';1000].iter());
+    keys2.extend(vec!['H';1].iter());
+    keys2.extend(vec!['K';1].iter());
+    keys2.extend(vec!['D';1].iter());
+    keys2.extend(vec!['O';1].iter());
+    keys2.extend(vec!['R';1].iter());
 
     keys1.push('B');
     keys2.push('B');
@@ -108,7 +108,7 @@ fn bench_matched(c : &mut Criterion) {
 
     c.bench_function(
         "Compare 2 Strings",
-        |b| b.iter(|| matched(&element.0,&element.1))
+        |b| b.iter(|| matched(element.0.clone(),element.1.clone()))
     );
 }
 
@@ -116,17 +116,17 @@ fn inuse_bench_matched(c : &mut Criterion) {
     let mut keys1 = String::from("A");
     let mut keys2 = String::from("A");
 
-    keys1.extend(vec!['H';1000].iter());
-    keys1.extend(vec!['K';1000].iter());
-    keys1.extend(vec!['D';1000].iter());
-    keys1.extend(vec!['O';1000].iter());
-    keys1.extend(vec!['R';1000].iter());
+    keys1.extend(vec!['H';1].iter());
+    keys1.extend(vec!['K';1].iter());
+    keys1.extend(vec!['D';1].iter());
+    keys1.extend(vec!['O';1].iter());
+    keys1.extend(vec!['R';1].iter());
 
-    keys2.extend(vec!['H';1000].iter());
-    keys2.extend(vec!['K';1000].iter());
-    keys2.extend(vec!['D';1000].iter());
-    keys2.extend(vec!['O';1000].iter());
-    keys2.extend(vec!['R';1000].iter());
+    keys2.extend(vec!['H';1].iter());
+    keys2.extend(vec!['K';1].iter());
+    keys2.extend(vec!['D';1].iter());
+    keys2.extend(vec!['O';1].iter());
+    keys2.extend(vec!['R';1].iter());
 
     keys1.push('B');
     keys2.push('B');
@@ -137,7 +137,7 @@ fn inuse_bench_matched(c : &mut Criterion) {
 
     c.bench_function(
         "Compare 2 Strings with in used",
-        |b| b.iter(|| in_matched(&element.0,&element.1))
+        |b| b.iter(|| in_matched(element.0.clone(),element.1.clone()))
     );
 }
 
@@ -145,17 +145,17 @@ fn builtin_bench_matched(c : &mut Criterion) {
     let mut keys1 = String::from("A");
     let mut keys2 = String::from("A");
 
-    keys1.extend(vec!['H';1000].iter());
-    keys1.extend(vec!['K';1000].iter());
-    keys1.extend(vec!['D';1000].iter());
-    keys1.extend(vec!['O';1000].iter());
-    keys1.extend(vec!['R';1000].iter());
+    keys1.extend(vec!['H';1].iter());
+    keys1.extend(vec!['K';1].iter());
+    keys1.extend(vec!['D';1].iter());
+    keys1.extend(vec!['O';1].iter());
+    keys1.extend(vec!['R';1].iter());
 
-    keys2.extend(vec!['H';1000].iter());
-    keys2.extend(vec!['K';1000].iter());
-    keys2.extend(vec!['D';1000].iter());
-    keys2.extend(vec!['O';1000].iter());
-    keys2.extend(vec!['R';1000].iter());
+    keys2.extend(vec!['H';1].iter());
+    keys2.extend(vec!['K';1].iter());
+    keys2.extend(vec!['D';1].iter());
+    keys2.extend(vec!['O';1].iter());
+    keys2.extend(vec!['R';1].iter());
 
     keys1.push('B');
     keys2.push('B');
@@ -166,7 +166,7 @@ fn builtin_bench_matched(c : &mut Criterion) {
 
     c.bench_function(
         "Compare 2 Strings with builtin lib",
-        |b| b.iter(|| builtin_matched(&element.0,&element.1))
+        |b| b.iter(|| builtin_matched(element.0.clone(),element.1.clone()))
     );
 }
 
